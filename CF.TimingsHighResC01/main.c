4adf737ee186913083ea360bf09a739ce595491e
Alan Sampson <alansamps@gmail.com>
2021-02-14 18:52:51 -0800

    Initial Commit

---
+++ CF.TimingsHighResC01/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CF.TimingsHighResC01
+//
+//  Created by Alan Sampson on 2/14/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
